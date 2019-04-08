3dda3bd470aa63c77a247a1b940b872e9ad4d24e
原田浩輝 <haradahiroki@haradahirokinoMacBook-Pro.local>
2019-04-08 21:50:39 +0900

    [Finish]3-6

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -3,19 +3,19 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
-  def create
-  	post = Post.new(post_params)
-  	post.save
-  	redirect_to '/top'
-  end
-
   def index
   	@posts = Post.all
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
+  def create
+  	post = Post.new(post_params)
+  	post.save
+  	redirect_to post_path(post.id)
+  end
   private
 
   def post_params
