be5b939889bffb5195d5346d38f9168c88a0316e
原田浩輝 <haradahiroki@haradahirokinoMacBook-Pro.local>
2019-04-09 14:17:38 +0900

    [Start]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9600d258059dbfb28c967ee450a27736b21b159b540f3404d385fd1ca00202186cecbc041d05749db45db267ce8d9a416b5f9ecd10c78f2447b7e835894e49e9'
